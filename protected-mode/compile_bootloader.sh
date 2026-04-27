6b2461df80c0516c257a7a7c0aa66d22271544ba
harmar <ttai33638@gmail.com>
2026-04-27 11:25:58 +0800

    restore /protected-mode which delete careless

---
+++ protected-mode/compile_bootloader.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# compile_bootloader.sh <source> <disk>
+
+src="$1"
+object="${src%.*}.o"
+bin="${src%.*}.bin"
+
+disk="$2"
+
+as --32 $src -o ./output/$object
+ld -m elf_i386 -Ttext 0x7c00 --oformat binary ./output/$object -o /output/$bin
+dd if=/output/$bin of=$disk bs=512 count=1 conv=notrunc
